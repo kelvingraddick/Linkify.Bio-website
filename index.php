fa794c5bc87dabde82c0107501306c249da09f65
Kelvin <kgraddick@wavelinkllc.com>
2017-07-03 00:06:36 -0400

    Adding keywords/features to home page

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
         <div class="col-xs-1 col-md-2"></div>
         <div class="col-xs-10 col-md-8 jumbotron_overlay">
             <div class="jumbotron_overlay_title">
-                Quickly provide multiple links in a social bio or ad through a single link!
+                <h1>Quickly provide multiple links in a social bio or ad through a single link!</h1>
             </div>
             <div class="jumbotron_overlay_buttons">
                 <button class="instagram_button" onclick="location.href='https://api.instagram.com/oauth/authorize/?client_id=4a18edf1ceff4747b2c06e38d3395b34&redirect_uri=https://linkify.bio/register/instagram.php&response_type=code';">
@@ -58,7 +58,7 @@
     </div>
 	<div class="container">
         <div class="row">
-			<div class="col-xs-12 col-md-12 preview">
+			<div class="col-xs-12 col-md-6 preview">
                 <div class="marvel-device iphone6 silver">
                     <div class="top-bar"></div>
                     <div class="sleep"></div>
@@ -72,6 +72,26 @@
                     <div class="home"></div>
                     <div class="bottom-bar"></div>
                 </div>
+			</div>
+            <div class="col-xs-12 col-md-6 features">
+                <table>
+                    <tr>
+                        <td><i class="fa fa-link" aria-hidden="true"></i></td>
+                        <td><h2>Provide <b>multiple links</b> to your followers in your Instagram/Twitter bio!</h2></td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-retweet" aria-hidden="true"></i></td>
+                        <td><h2>Share all you important links and other social profiles with a <b>single URL!</b></h2></td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
+                        <td><h2>Personalized, short, bio link (ex. <b>linkify.bio/100kelvins/</b>) that you never have to change!</h2></td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-star" aria-hidden="true"></i></td>
+                        <td><h2>Completely <b>FREE</b>! Sign up in less than 1 minute with your Instagram account!</h2></td>
+                    </tr>
+                </table>
 			</div>
 		</div>
 	</div>
